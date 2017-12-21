8421b77137d39eecf386fe02b2d0f62a06e129af
Benjamin Bowes <bbowes999@gmail.com>
2017-12-21 10:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
-  return `returns "It\'s ${days} days until ${holiday}!`;
+  return `It\'s ${days} days until ${holiday}!`;
 }
